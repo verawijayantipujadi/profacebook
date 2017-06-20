9a4587d49952ebb332d15a2583a49537bbbe985a
verawijayantipujadi <vera.wijay1806@gmail.com>
2017-06-20 10:44:15 +0700

    Add files via upload

---
+++ PostsFromPageExtractor/PostsFromPageExtractor.java
@@ -0,0 +1,60 @@
+import facebook4j.Comment;
+import facebook4j.Facebook;
+import facebook4j.FacebookException;
+import facebook4j.FacebookFactory;
+import facebook4j.PagableList;
+import facebook4j.Post;
+import facebook4j.Reading;
+import facebook4j.ResponseList;
+import facebook4j.auth.AccessToken;
+
+public class PostsFromPageExtractor {
+
+/**
+ * A simple Facebook4J client which
+ * illustrates how to access group feeds / posts / comments.
+ * 
+ * @param args
+ * @throws FacebookException 
+ */
+public static void main(String[] args) throws FacebookException {
+
+    // Generate facebook instance.
+    Facebook facebook = new FacebookFactory().getInstance();
+    // Use default values for oauth app id.
+    facebook.setOAuthAppId("", "");
+    // Get an access token from: 
+    // https://developers.facebook.com/tools/explorer
+    // Copy and paste it below.
+    String accessTokenString = "EAACEdEose0cBAPJkBZCVguaqrT8kZApKdHyouRCwZCzKtBRH5CqzZAiiafidMgLv2Lzp82nokiQ0fGc6WMRGsdsmXijUvDt9uXRqwf0Jn3ML6qqwLHa9Q3i6CkxsgQOrZCev6XA4y91B2DM4U8NqxbEqLP6ZBejDGelzC51CbIUEyZCxLe2YzqsmDBVq2ZAsN5sZD";
+    AccessToken at = new AccessToken(accessTokenString);
+    // Set access token.
+    facebook.setOAuthAccessToken(at);
+
+    // We're done.
+    // Access group feeds.
+    // You can get the group ID from:
+    // https://developers.facebook.com/tools/explorer
+
+    // Set limit to 25 feeds.
+    /*ResponseList<Post> feeds = facebook.getFeed("187446750783",
+            new Reading().limit(25));
+
+        // For all 25 feeds...
+        for (int i = 0; i < feeds.size(); i++) {
+            // Get post.
+            Post post = feeds.get(i);
+            // Get (string) message.
+            String message = post.getMessage();
+                            // Print out the message.
+            System.out.println(message);
+
+            // Get more stuff...
+            PagableList<Comment> comments = post.getComments();
+            String date = post.getCreatedTime().toString();
+            String name = post.getFrom().getName();
+            String id = post.getId();
+        } */    
+         facebook.postStatusMessage("Hello World from Facebook4J.");     
+    }
+}
